35ded1ca3b2c4164321601717e0685c9f8e7120f
carti69 <zhukow28@gmail.com>
2025-05-23 12:17:40 -0600

    trying to make a way to exit the program without acessing terminal and ctrl+c

--- image_script.py
+++ image_script.py
@@ -4,7 +4,7 @@ import random
 import time
 import sys
 
-from pygame import K_ESCAPE
+from pygame import K_ESCAPE, K_LCTRL
 
 image_folder = "/home/pi-guest-user/share"
 #these dimensions are for a 1080p monitor
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     running = True
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.K_ESCAPE:
+            if event.type == pygame.KEYDOWN and (event.key == K_ESCAPE and event.key == K_LCTRL):
                 running = False
 
         display_images(current_screen, image_list[current_image_index % num_images])
